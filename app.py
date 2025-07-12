37030bf8a167c7d9a361379ce760fbd1d9ef22b4
Testforeverything <testforeverything5665@gmail.com>
2025-07-12 11:33:21 +0700

    app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ if menu == "Risk Overview":
                 loan_dist,
                 names="Loan Type",
                 values="Count",
-                hole=0.1,
+                hole=0.0,
                 title="Loan Type Breakdown",
                 color_discrete_sequence=flowen_colors
             )
